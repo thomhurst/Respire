2215f71b4df905adf20b8ee7aae7cea979ba2fe0
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-08-28 10:52:39 +0100

    Update NuGet package references and project configuration
    - Updated package versions in project.nuget.cache to the latest stable releases.
    - Modified project.packagespec.json to reflect changes in package dependencies and versions.
    - Adjusted rider.project.model.nuget.info and rider.project.restore.info with new identifiers.

--- src/Respire/obj/Debug/net8.0/Respire.AssemblyInfo.cs
+++ src/Respire/obj/Debug/net8.0/Respire.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Respire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d8c3623f61e157ed607c13f1e65ef722fe5318c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8cb42698b3ac571e41364d52a768f007b10835")]
 [assembly: System.Reflection.AssemblyProductAttribute("Respire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Respire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/Respire.IntegrationTests/obj/Debug/net8.0/Respire.IntegrationTests.GlobalUsings.g.cs
+++ tests/Respire.IntegrationTests/obj/Debug/net8.0/Respire.IntegrationTests.GlobalUsings.g.cs
@@ -6,4 +6,3 @@ global using System.Linq;
 global using System.Net.Http;
 global using System.Threading;
 global using System.Threading.Tasks;
-global using Xunit;
