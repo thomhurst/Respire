a498617821de6b614cf65f4e0c82f14192d1aa69
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-08-09 13:23:34 +0100

    Fix ModularPipelines v3 pipeline migration

--- Respire.Pipeline/Modules/LocalMachine/AddLocalNugetSourceModule.cs
+++ Respire.Pipeline/Modules/LocalMachine/AddLocalNugetSourceModule.cs
@@ -1,9 +1,6 @@
 using Microsoft.Extensions.Logging;
 using ModularPipelines.Attributes;
 using ModularPipelines.Context;
-using ModularPipelines.DotNet.Options;
-using ModularPipelines.DotNet.Extensions;
-using ModularPipelines.DotNet.Options;
 using ModularPipelines.Models;
 using ModularPipelines.Modules;
 
@@ -12,9 +9,9 @@ namespace Respire.Pipeline.Modules.LocalMachine;
 [DependsOn<CreateLocalNugetFolderModule>]
 public class AddLocalNugetSourceModule : Module<CommandResult>
 {
-    protected override async Task<CommandResult> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
+    protected override async Task<CommandResult?> ExecuteAsync(IModuleContext context, CancellationToken cancellationToken)
     {
-        var localNugetFolder = await GetModule<CreateLocalNugetFolderModule>();
+        _ = await context.GetModule<CreateLocalNugetFolderModule>();
         
         context.Logger.LogInformation("Adding local NuGet source...");
         
